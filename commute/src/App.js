83ee79cce8f9d545032264b5595b701982c03686
bram <brampijper@gmail.com>
2018-02-13 15:59:43 +0100

    Fixed reducers path

--- commute/src/App.js
+++ commute/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
 import Router from './Router';
+import reducers from './reducers';
 
 class App extends Component {
     render() {
