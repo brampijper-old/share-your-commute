4ca7d868e64cc9a9705e84a26209bda9963b0003
bram <brampijper@gmail.com>
2018-02-15 18:03:04 +0100

    Changed the styling of the input field

--- commute/src/components/common/Input.js
+++ commute/src/components/common/Input.js
@@ -26,18 +26,21 @@ const styles = {
     paddingLeft: 5,
     fontSize: 18,
     lineHeight: 23,
-    flex: 2
+    height: 50,
+    flex: 1,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: 'red'
   },
   labelStyle: {
     fontSize: 18,
-    paddingLeft: 20,
+    paddingLeft: 5,
     flex: 1
   },
   containerStyle: {
-    height: 40,
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center'
+    height: 80,
+    flex: 0.8,
+    justifyContent: 'space-between'
   }
 };
 
