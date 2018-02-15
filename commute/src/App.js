de4010ddb67978ea1cd84a6c83e2299e8d22d0bc
bram <brampijper@gmail.com>
2018-02-15 12:28:57 +0100

    Added firebase config

--- commute/src/App.js
+++ commute/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import Router from './Router';
 import reducers from './reducers';
 
 class App extends Component {
+    componentDidMount() {
+        const config = {
+            apiKey: 'AIzaSyB8stcgmNfrlQS2SFszPWKShESWiBELFR8',
+            authDomain: 'commuting-47a30.firebaseapp.com',
+            databaseURL: 'https://commuting-47a30.firebaseio.com',
+            projectId: 'commuting-47a30',
+            storageBucket: '',
+            messagingSenderId: '364363747467'
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
       const store = createStore(reducers, {}, applyMiddleware(ReduxThunk)); 
         return (
