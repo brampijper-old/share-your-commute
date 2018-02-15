de871e796121032eb3dde45e80094b7db6a92823
bram <brampijper@gmail.com>
2018-02-15 18:01:57 +0100

    Added scene for the register page

--- commute/src/Router.js
+++ commute/src/Router.js
@@ -1,12 +1,20 @@
 import React from 'react'; 
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root" hideNavBar>
-                <Scene key="login" component={LoginForm} title="Please Login" initial />
+
+                <Scene key="start">
+                    <Scene key="startOptions" component={LoginForm} initial />
+                </Scene>
+
+                <Scene key="register">
+                    <Scene key="registerStart" component={RegisterForm} />
+                </Scene>
             </Scene>
         </Router>
     );
