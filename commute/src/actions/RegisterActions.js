342ad2955d7ce277d5a3eda1ff54ce5dfe1f8f46
bram <brampijper@gmail.com>
2018-02-16 17:00:43 +0100

    Added an extra check if a user already exists

---
+++ commute/src/actions/RegisterActions.js
@@ -0,0 +1,56 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_START,
+    LOGIN_USER_FAIL,
+    LOGIN_USER_SUCCESS,
+} from './types';
+
+export const emailChanged = (text) => {
+    return {
+        type: EMAIL_CHANGED,
+        payload: text
+    };
+};
+
+export const passwordChanged = (text) => {
+    return {
+        type: PASSWORD_CHANGED,
+        payload: text
+    };
+};
+
+export const loginUser = ({ email, password }) => {
+    return (dispatch) => {
+        dispatch({ type: LOGIN_USER_START }); 
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => {
+                loginUserSucces(dispatch, user);
+                Actions.basicInfo(); 
+            })
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then((user) => {
+                        loginUserSucces(dispatch, user);
+                        Actions.basicInfo();    
+                    })
+                .catch((error) => console.log(error), loginUserFail(dispatch));
+            });
+    };
+}; 
+
+const loginUserFail = (dispatch) => {
+    dispatch({
+        type: LOGIN_USER_FAIL
+    });
+};
+
+const loginUserSucces = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
