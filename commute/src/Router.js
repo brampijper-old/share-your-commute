50a6c1c95b993f147e04ed84b0f0a18abfd7233f
bram <brampijper@gmail.com>
2018-02-16 16:49:54 +0100

    Added basic info scene

--- commute/src/Router.js
+++ commute/src/Router.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
+import BasicInfoForm from './components/BasicInfoForm';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root" hideNavBar>
 
-                <Scene key="start">
+                <Scene key="start" hideNavBar>
                     <Scene key="startOptions" component={LoginForm} initial />
                 </Scene>
 
-                <Scene key="register">
+                <Scene key="register" hideNavBar>
                     <Scene key="registerStart" component={RegisterForm} />
+                    <Scene key="basicInfo" component={BasicInfoForm} />
                 </Scene>
             </Scene>
         </Router>
